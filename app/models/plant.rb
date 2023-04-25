6482bdbccf736f1d6362d2c82ab455e6248b0a2c
Amy Spears <amspears007@gmail.com>
2023-04-25 11:52:19 -0600

    Remove model methods from plant and plot

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,8 +1,4 @@
 class Plant < ApplicationRecord
   has_many :plot_plants
   has_many :plots, through: :plot_plants
-
-  def self.less_than_100_harvest
-    Plant.where('days_to_harvest < 100')
-  end
 end
--- app/models/plot.rb
+++ app/models/plot.rb
@@ -3,9 +3,6 @@ class Plot < ApplicationRecord
   has_many :plot_plants
   has_many :plants, through: :plot_plants
 
-  def less_than_100_harvest
-    Plant.where('days_to_harvest < 100').distinct
-  end
 end
 
 
