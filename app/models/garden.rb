8d48a0b8f69a2e7aceee1ab27e9343e36027f7ef
Amy Spears <amspears007@gmail.com>
2023-04-25 11:42:17 -0600

    Add garden model method

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,8 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+
+  def less_than_100_harvest
+    Plant.where('days_to_harvest < 100').distinct
+  end
 end
--- app/models/plot.rb
+++ app/models/plot.rb
@@ -2,4 +2,10 @@ class Plot < ApplicationRecord
   belongs_to :garden
   has_many :plot_plants
   has_many :plants, through: :plot_plants
+
+  def less_than_100_harvest
+    Plant.where('days_to_harvest < 100').distinct
+  end
 end
+
+
